996ae27a605aac4f2d23143a00d92e7cda704168
Imdal <imdal1220@gmail.com>
2021-12-05 13:51:08 +0100

    complete

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
 import crypto from 'crypto';
+import * as math from 'mathjs'
 
-function generateRandomPrime() {
-    let DH = crypto.createDiffieHellman(16);
-
+function generateRandomPrime(length) {
+    let DH = crypto.createDiffieHellman(length);
     let prime = DH.getPrime('hex');
-    return parseInt(prime, 16);
+    return parseInt(prime, length);
 }
 
 function generateRandomBit() {
     return Math.round(Math.random());
 }
 
+class User {
+    constructor(g, n) {
+        this.n = n;
+        this.g = g
+    }
+
+    PsetX(x) {
+        this.x = x
+    }
+
+    VsetY(y) {
+        this.y = y
+    }
+
+    Pstep1() {
+        this.v = Math.floor(Math.random() * 100)
+        this.t = (this.g ** this.v) % this.n;
+        return this.t
+    }
+
+    Vstep1(t) {
+        this.t = t
+        this.c = generateRandomBit()
+        return this.c
+    }
+
+    Pstep2(c) {
+        this.r = this.v - (c * this.x); //?
+        return this.r
+    }
+
+    Vstep2(r) {
+        if(this.t === ((this.g**r)*(this.y**this.c))%this.n)
+            return true
+        else
+            return false
+    }
 
 
-let p = generateRandomPrime();
-console.log(p);
-let q = generateRandomPrime();
-console.log(q);
-let n = p * q;
-console.log(n);
-
-// P:
-let r = Math.floor(Math.random()*100);
-
-let t = (r**2) % n;
-
-// V:
-let c = generateRandomBit();
-console.log(c);
-
-// P:
-let s = (r*(x**c))%n;
-
-// V:
-if(s**2 % n == t*y**c % n) {
-    return true;
 }
+
+let n = generateRandomPrime(16)
+let g = Math.floor(Math.random() * 50)
+console.log("n: ", n)
+console.log("g: ", g)
+
+let P = new User(g, n)
+let x = 50
+P.PsetX(x)
+console.log("x: ", x)
+let V = new User(g, n)
+let y = g**x
+V.VsetY(y)
+console.log("y: ", y)
+
+let t = P.Pstep1()
+console.log("t: ", t)
+let c = V.Vstep1(t)
+console.log("c: ", c)
+let r = P.Pstep2(c)
+console.log("r: ", r)
+if(V.Vstep2(r))
+    console.log("Sikeres")
 else
-    return false;
+    console.log("Sikertelen")
